0176b8d705d50f745c4f63e6ced72540907854c6
Wenphing Lo <wlo2@mmm.com>
2015-08-09 22:55:34 -0700

    SpringBatch

---
+++ src/main/java/com/mmm/batch/Tasklet2.java
@@ -0,0 +1,17 @@
+package com.mmm.batch;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class Tasklet2 implements Tasklet {
+
+	public RepeatStatus execute(StepContribution arg0, ChunkContext arg1)
+			throws Exception {
+
+		System.out.println("Tasklet2 is in the house !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		return RepeatStatus.FINISHED;
+	}
+
+}
